1586ef5ae1d94c3132fb52cb7d67215d1ce99b26
yonggyo1125 <yonggyo00@kakao.com>
2024-12-24 17:59:55 +0900

    .

--- js/todo.js
+++ js/todo.js
@@ -86,6 +86,7 @@ const todo = {
           const done = this.value === "true";
           todo.items[itemIndex].done = done;
           todo.render();
+          todo.save();
         });
       }
     }
